c351df9d1cfa2e557fe856a1a01fa0a24594c852
Rabin Acharya <charms2074@gmail.com>
2023-06-03 20:27:10 +0545

    array in java with for and enhanced for loop

---
+++ src/Array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+
+        // scanner for user input
+        Scanner in = new Scanner(System.in);
+// declaring the array
+        int[] arr = new int[5]; // arr size => 5
+        // taking the input from the user iterating the loop for the size of array
+        // using for loop
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(arr.toString());
+
+        // using enhanced for loop printing the values of the array
+
+        for (int element : arr) {
+
+
+            System.out.println(element); // prints the element and breaks the line after done printing
+        }
+    }
+}
