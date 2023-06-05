d4d4525f6f40f9e64a4b024db47e7d2816e93ad4
Rabin Acharya <charms2074@gmail.com>
2023-06-05 23:51:22 +0545

    linear search in array done

---
+++ src/LinearSearch.java
@@ -0,0 +1,32 @@
+
+// find if 14 exits  of not 14 is in the array or not
+public class LinearSearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,5,6,87,14,45,6,7,8};
+        int target = 45 ;
+        System.out.println(searchArray(arr, target));
+
+    }
+
+
+    static boolean searchArray(int[] arr, int target) {
+
+if(arr.length ==0) {
+
+return false ;
+}
+
+
+for(int element : arr) {
+
+if(element == target) {
+
+return true;
+}
+}
+        return false ;
+    }
+
+}
+
