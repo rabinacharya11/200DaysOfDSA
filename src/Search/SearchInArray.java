19ae0d9e84896d3ef7429232be33e68d2a8cfb44
Rabin Acharya <charms2074@gmail.com>
2023-06-06 00:11:38 +0545

    search in range done

--- src/Search/SearchInArray.java
+++ src/Search/SearchInArray.java
@@ -11,14 +11,15 @@ public class SearchInArray {
     }
 
     public  static boolean searchInArray(String name , char target) {
-
+// if there is an empty name, we return false
         if(name.isEmpty()) {
         return false;
         }
+        // if not empty name is passed we will make sure to search everyCharacters that are in the String
 
         for (int i = 0; i<name.length() ; i++) {
 
-
+// checking
             if(name.charAt(i) == target) {
 
 
@@ -26,7 +27,6 @@ public class SearchInArray {
         }
 
         return false ;
-
 
     }
 }
---
+++ src/Search/SearchInRange.java
@@ -0,0 +1,39 @@
+package Search;
+
+public class SearchInRange {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 6, 7, 7, 8, 99, 100, 234, 45, 66, 7, 8, 8, 9};
+        int target = 2;
+        int end = 5;
+        int begin = 0;
+
+        System.out.print(searchInRange(arr, target, end, begin));
+
+
+    }
+
+    public static boolean searchInRange(int[] arr, int target, int end, int begin) {
+
+        if (arr.length == 0) {
+            return false;
+        }
+
+
+        if (end < begin) {
+
+            return false; //here we are return -1 as well but the return type of the current function is boolean
+        }
+
+        for (int element : arr) {
+            if (element == target) {
+                return true;
+            }
+
+
+
+
+
+        }
+        return  false;
+    }
+}
