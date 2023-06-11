c08f1d24495cc4ffe7cf78d20ad253768a0ba251
Rabin Acharya <charms2074@gmail.com>
2023-06-12 00:14:11 +0545

    max value done in 2D array

--- src/Search/MinMaxValue2D.java
+++ src/Search/MinMaxValue2D.java
@@ -6,10 +6,10 @@ public class MinMaxValue2D {
         int[][] arr = {
                 { 3, 2, 1, 9 },
                 { 10, 11, 23, 45 },
-                { 34, 45, 56, -78 },
+                { 34, 45, 56, 78 },
         };
 
-        System.out.println(minValue(arr));
+        System.out.println(maxVaue(arr));
     }
 
     public static int minValue(int[][] arr) {
@@ -27,5 +27,22 @@ public class MinMaxValue2D {
         }
 
         return minValue;
+    }
+
+    public static int maxVaue(int[][] arr) {
+        int maxValue = arr[0][0];
+
+        for (int row = 0; row < arr.length; row++) {
+
+            for (int col = 0; col < arr[row].length; col++) {
+                int currentValue = arr[row][col];
+                if (currentValue > maxValue) {
+                    maxValue = currentValue;
+                }
+
+            }
+        }
+
+        return maxValue;
     }
 }
