fdfbbea13b17db6f1903209d08017cf240e682d0
Rabin Acharya <charms2074@gmail.com>
2023-06-11 23:58:55 +0545

    min and max in range done

--- src/Search/MinMaxValue.java
+++ src/Search/MinMaxValue.java
@@ -5,7 +5,7 @@ public class MinMaxValue {
     public static void main(String[] args) {
 
         int[] arr = { 100, 2, 3, 4, 500, 6, 7, 8, 9 };
-        System.out.println(intMinValue(arr));
+        System.out.println(intMinValueInRange(arr, 6, 8));
 
     }
 
@@ -34,6 +34,18 @@ public class MinMaxValue {
             }
         }
         return min;
+
+    }
+
+    public static int intMinValueInRange(int[] arr, int start, int end) {
+        int data = arr[start];
+        for (int i = start; i <= end; i++) {
+            if (arr[i] < data) {
+                data = arr[i];
+            }
+
+        }
+        return data;
 
     }
 
